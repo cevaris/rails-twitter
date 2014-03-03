6f2598db445634707a271ee915ea8cfc391d0d1f
Cevaris <cevaris@gmail.com>
2014-03-02 22:42:08 -0700

    limited consume_events to 1 worker

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,7 +3,11 @@ require 'jobs'
 Resque.redis = Redis.new(Rq::Application.config.redis)
 
 
-# Delete all queued workers for consuming events
-Resque.redis.del "queue:consume_events"
-# Start workers for consuming events
-Resque.enqueue(Jobs::ConsumeEvents, {channel: EventApplication::RAW_EVENTS})
+# Starts only one worker
+if Resque.size('consume_events') == 0
+  puts "Start Consumer #{Resque.size('consume_events')} "
+  Resque.enqueue(Jobs::ConsumeEvents, {channel: EventApplication::RAW_EVENTS})
+else
+  # Delete any queued consumers
+  Resque.redis.del "resque:queue:consume_events"
+end
