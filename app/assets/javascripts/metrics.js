df211ecb31b4ee73d900cf140ff9302760862cad
Cevaris <cevaris@gmail.com>
2014-03-19 14:02:56 -0600

    created switch statement to render different objects

--- app/assets/javascripts/metrics.js
+++ app/assets/javascripts/metrics.js
@@ -6,16 +6,53 @@ function Metrics( options ) {
   }, options );
 
 
-  this.handle = function (type, object) {
-
-  };
+  
 
   this.render = function () {
     console.log("rendering");   
 
     if (settings.data == undefined) {
-      throw Error("No data found");
+      throw Error("Data object is null.");
     }
+    if (settings.data.documents == undefined) {
+      throw Error("Data documents is null.");
+    }
+
+    var data = settings.data,
+        documents = settings.data.documents;
+
+    documents.forEach(function(element) {
+
+      switch (element.render) {
+        case "map":
+          renderMap(element);
+          break;
+        case "text":
+          renderText(element);
+          break;
+        case "table":
+          renderTable(element);
+          break;
+        default:
+          throw Error("Could not find render type "+data.render);
+      }
+
+    });
+    
+
+  };
+
+  var renderMap = function (data) {
+    console.log("Rendering Map");
+    console.log(data);
+  };
+  var renderText = function (data) {
+    console.log("Rendering Text");
+    console.log(data);
+  };
+  var renderTable = function (data) {
+    console.log("Rendering Table");
+    console.log(data);
   };
 
 
